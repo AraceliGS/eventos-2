312e88f8afb8cac3eebac68648030e028ac73517
AraceliGS <aragutarras@gmail.com>
2017-11-14 18:17:21 -0500

    añadiendo un segundo archivo html

---
+++ js/app.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function(){
+    var divContainer = document.getElementById('div1');
+    var link = divContainer.firstElementChild;
+    console.log(link);
+    console.log(link.attributes);
+    console.log(link.href);
+})
